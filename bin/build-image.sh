b3ea6dc3891eca93fc6e91fdb68e503c4b711895
andrewbackes <rbackes@ucla.edu>
2017-11-13 21:08:38 -0800

    Ignor creds

--- bin/build-image.sh
+++ bin/build-image.sh
@@ -6,52 +6,12 @@ mkdir -p build
 yarn install
 yarn build
 
-cat <<EOF > build/localhost.conf
-server {
-    listen 8080;
-    server_name localhost;
-    root /usr/share/nginx/html;
-
-    gzip_static on;
-
-    #location / {
-    #    # Set path
-    #    
-    #    try_files $uri /index.html =404;
-    #}
-}
-EOF
-
-cat <<EOF > build/tourney.aback.es.conf
-server {
-    listen 80;
-    server_name tourney.aback.es;
-    root /usr/share/nginx/html;
-
-    gzip_static on;
-
-    #location / {
-    #  try_files $uri $uri/ /index.html;
-    #}
-
-    # Attempt to load static files, if not found route to @rootfiles
-    #location ~ (.+)\.(html|json|txt|js|css|jpg|jpeg|gif|png|svg|ico|eot|otf|woff|woff2|ttf)$ {
-    #  try_files $uri @rootfiles =404;
-    #}
-
-    # Check for app route "directories" in the request uri and strip "directories"
-    # from request, loading paths relative to root.
-    #location @rootfiles {
-    #  rewrite ^/(?:foo/bar/baz|foo/bar|foo|tacos)/(.*) /$1 redirect;
-    #}
-}
-EOF
-
 cat <<EOF > build/Dockerfile-ui
-FROM nginx:1.13.6
-COPY build /usr/share/nginx/html
-COPY build/tourney.aback.es.conf /etc/nginx/conf.d/tourney.aback.es.conf
-COPY build/localhost.conf /etc/nginx/conf.d/localhost.conf
+FROM node
+RUN yarn global add serve
+COPY build /build
+WORKDIR /
+CMD ["serve", "-p", "80", "-s", "build"]
 EOF
 
 TAG=$(git rev-parse --short HEAD)
